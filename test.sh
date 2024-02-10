214fd7c89054c5dc1532272c10c6a1080dc235ff
Victor Ifah <97197194+GerromeSieger@users.noreply.github.com>
2024-02-11 00:52:46 +0100

    pipeline

---
+++ test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install -y nginx
+
+sudo rm -rf /var/www/html/*
+sudo cp -r build/* /var/www/html/
+
+sudo systemctl start nginx
+
+sleep 5 
+
+STATUS_CODE=$(curl -LI http://localhost | grep -i http | awk '{print $2}')
+if [ $STATUS_CODE -eq 200 ]; then
+  echo "Success! Got 200 response from local server"
+  exit 0
+else
+  echo "Failed! Expected 200 response, got $STATUS_CODE" 
+  exit 1
+fi
+
+sudo systemctl stop nginx
