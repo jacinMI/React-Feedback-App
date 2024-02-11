3a818e8cc4b02934892c21cac7145d0b52748fd8
Victor Ifah <97197194+GerromeSieger@users.noreply.github.com>
2024-02-11 01:07:47 +0100

    edits

--- test.sh
+++ test.sh
@@ -10,12 +10,22 @@ sudo systemctl start nginx
 
 sleep 5 
 
+RESPONSE=$(curl -s http://localhost)
+
 STATUS_CODE=$(curl -LI http://localhost | grep -i http | awk '{print $2}')
 if [ $STATUS_CODE -eq 200 ]; then
   echo "Success! Got 200 response from local server"
   exit 0
 else
   echo "Failed! Expected 200 response, got $STATUS_CODE" 
+  exit 1
+fi
+
+if echo $RESPONSE | grep -q "Feedback UI"; then
+  echo "Success! Got expected response from local server"
+  exit 0  
+else
+  echo "Failed! Did not find expected 'Feedback UI' text"
   exit 1
 fi
 
